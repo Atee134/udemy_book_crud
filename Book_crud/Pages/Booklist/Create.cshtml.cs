3825d1131b56020e6f1a11dd63fa70deb9e002bf
lorincz_attila <l.atee.1995@gmail.com>
2018-12-12 19:54:44 +0100

    end of 21

---
+++ Book_crud/Pages/Booklist/Create.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Book_crud.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace Book_crud.Pages.Booklist
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Book Book { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Books.Add(Book);
+
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
